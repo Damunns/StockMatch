444e1aec30f3147b7ede2740bdad35c32369fe74
Damunns <munnzed@gmail.com>
2026-04-11 22:37:53 +0100

    Add project files.

---
+++ StockMatch/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace StockMatch.Models
+{
+    public class Users
+    {
+        public required string EmailUsername { get; set; }
+        public string? LogonUsername { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Access {  get; set; }
+
+    }
+}
---
+++ StockMatch/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using StockMatch.Models;
+
+public class UserService
+{
+    private readonly string _connectionString;
+
+    public UserService(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DEVConnection")
+                            ?? throw new Exception("Connection string not found");
+    }
+
+    // Get all users
+    public async Task<IEnumerable<Users>> GetAllUsers()
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var rawData = await db.QueryAsync<Users>("SELECT * FROM dbo.Users");
+            var firstRow = rawData.FirstOrDefault();
+            return rawData;
+        }
+    }
+
+    // Gets User Details
+    public async Task<Users?> GetUserDetails(string Username)
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var sql = "SELECT * FROM dbo.Users WHERE LogonUsername = @LogonUsername";
+            return await db.QueryFirstOrDefaultAsync<Users>(sql, new { LogonUsername = Username });
+        }
+    }
+}
+
